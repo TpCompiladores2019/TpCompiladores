fa3ebf37401b1743490fb6011a20679e4df514b0
Juan Stampone <juanstampone@gmail.com>
2019-11-01 17:34:57 -0300

    Mejoras en el float

--- src/main/java/AccionesSemanticas/ASFinalFloat.java
+++ src/main/java/AccionesSemanticas/ASFinalFloat.java
@@ -11,13 +11,20 @@ public class ASFinalFloat extends IAccionSemantica{
 	public int ejecutar(char caracter, StringBuilder cadena, TablaTokens tablaTokens, TablaSimbolos tablaSimbolos) {
 		AnalizadorLexico.indiceLectura--;
 		Double numeroFloat = Double.parseDouble(cadena.toString());
-		
-		if ((numeroFloat<Float.MIN_NORMAL && numeroFloat > Float.MAX_VALUE) || numeroFloat != 0.0) {
-			Error nuevoError = new Error("El numero " + cadena.toString() + " excede el rango de los Float",AnalizadorLexico.nroLinea," ","WARNING");
-			AnalizadorLexico.listaWarning.add(nuevoError);
+		if (numeroFloat > Float.MAX_VALUE) {
+			Error nuevoWarning = new Error("El numero " + cadena.toString() + " excede el rango de los Float, fue truncado a " + Float.MAX_VALUE +".",AnalizadorLexico.nroLinea," ","WARNING");
+			AnalizadorLexico.listaWarning.add(nuevoWarning);
 			cadena.delete(0, cadena.length());
 			cadena.append(String.valueOf(Float.MAX_VALUE));
 		}
+		else
+			if ((numeroFloat<Float.MIN_NORMAL) && (numeroFloat != 0.0)) {
+				Error nuevoWarning = new Error("El numero " + cadena.toString() + " excede el rango de los Float, fue truncado a " + Float.MIN_NORMAL + ".",AnalizadorLexico.nroLinea," ","WARNING");
+				AnalizadorLexico.listaWarning.add(nuevoWarning);
+				cadena.delete(0, cadena.length());
+				cadena.append(String.valueOf(Float.MIN_NORMAL));
+			}
+		
 		tablaSimbolos.agregar(cadena.toString(),new Registro("float"));
 		AnalizadorLexico.listaCorrectas.add("Linea " +AnalizadorLexico.nroLinea + " Constante Float: " + cadena.toString());
 		return tablaTokens.getToken("CONSTANTE F");
